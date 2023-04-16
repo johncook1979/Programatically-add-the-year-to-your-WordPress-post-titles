db4f88ef8cce8cbd96c44c1bfc07d4e8553547a8
John Cook <41190295+johncook1979@users.noreply.github.com>
2023-04-17 09:18:29 +1000

    Update functions.php

--- functions.php
+++ functions.php
@@ -21,3 +21,40 @@ function my_shortcode_title( $title ){
     // Here we simply run the WP do_shortcode() function with the $title atribute
     return do_shortcode( $title );
 }
+
+
+/**
+ * Update the slug to remove the year shortcode from the slug
+ *
+ * Hook into the transition post status function 
+ *
+*/
+add_action( 'transition_post_status', 'my_slug_update', 10, 3 );
+function my_slug_update( $new_status, $old_status, $post ){
+    // Check the value of the new and old status
+    if ( 'publish' !== $new_status or 'publish' === $old_status )
+        return;
+
+    // Define the slug based on the post title stripping special characters
+    $new_slug = sanitize_title( $post->post_title );
+
+    // Set the year (shortcode name) as the needle to find
+    $needle   = 'year';
+
+    // Check if the new slug contains the year
+    if (strpos($new_slug, $needle) !== false) {
+
+        // Here we replace the year shortcode with an empty value. You can modify as needed but read the readme for the reasoning behing an empty value
+        $new_slug = str_replace($needle, '', $new_slug);
+    }
+
+    // If the current slug does not match the new slug update the post on publish with the new slug
+    if ( $post->post_name != $new_slug ){
+        wp_update_post(
+            array (
+                'ID'        => $post->ID,
+                'post_name' => $new_slug
+            )
+        );
+    }
+}
